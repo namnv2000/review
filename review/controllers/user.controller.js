4c1b4fd680a769b145877387ba2073f94687b7ec
namnv <namnv.bhsoft@gmail.com>
2020-08-16 20:26:56 +0700

    update

--- review/controllers/user.controller.js
+++ review/controllers/user.controller.js
@@ -1,49 +1,31 @@
 const jwt = require("jsonwebtoken")
 const User = require("../modal/user.modal")
-const dotenv = require("dotenv");
-dotenv.config();
-const {registerValidation , loginValidation} = require("../auth/validation")
 
 module.exports.login = async function(req, res){
-    const{ error } = loginValidation(req.body);
-     if(error) return res.status(422).send(error.details[0].message)
-    const userLogin = await User.findOne({email: req.body.email});
-    if(!userLogin) return res.status(422).send(error.details[0].message)
-    const passLogin = await User.findOne({password: req.body.password});
-    if(!passLogin) return res.status(422).send(error.details[0].message)
-    var token = generateAccessToken({email: userLogin.email})
-    // res.header("auth-token", token).send(token);
-    res.status(200).send({
-        id: userLogin._id,
-        email: userLogin.email,
-        accessToken: token,
-        username: userLogin.name,
-        bio: userLogin.bio,
-        image: userLogin.image
-        });
-    };
+    try {
+        const { email, password } = req.body
+        const user = await User.findByCredentials(email, password)
+        if (!user) {
+            return res.status(401).send({error: 'Login failed! Check authentication credentials'})
+        }
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (error) {
+        res.status(400).send({ error: error.message })
+    }
+};
 
 module.exports.register  = async function(req, res){
-    const{ error } = registerValidation(req.body);
-    if(error) return res.status(422).send(error.details[0].message)
-    const newuser = new User();
-    newuser.name = req.body.name
-    newuser.email = req.body.email
-    newuser.password = req.body.password
-    newuser.bio = req.body.bio
-    newuser.image = req.body.image
-    try{
-        const user = await newuser.save()
-        res.send(user);
-    }catch(err){
-        res.status(422).send(err);
+    try {
+        const user = new User(req.body)
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (error) {
+        res.status(400).send(error)
     }
 };
 
 module.exports.Verify =  function(req, res){
-    res.send("tested successfully")
-}
-
-function generateAccessToken(id) {
-    return jwt.sign(id, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+    res.send(req.user)
 }
--- review/routes/index.js
+++ review/routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const verify = require("../auth/checkToken")
+const auth = require('../auth/auth')
 var controller = require('../controllers/user.controller');
 
 router.post('/api/user', controller.register)
-router.get('/', verify,  controller.Verify)
 
 router.post('/api/user/login', controller.login)
-module.exports = router;
+
+router.get('/', auth, controller.Verify)
+module.exports = router
