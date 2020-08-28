45065d45f8f4728490939225dd3253de622041c9
namnv <namnv.bhsoft@gmail.com>
2020-08-28 10:58:48 +0700

    middleware

--- review/routes/index.js
+++ review/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require('../middleware/VerifyToken')
+const auth = require('../middlewares/VerifyToken')
 var controller = require('../controllers/user.controller');
 
 router.post('/users', controller.register)
